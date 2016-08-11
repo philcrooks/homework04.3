9f994aefc71df536565fb886e51b2e0131c2f6fb
Phil Crooks <pacman@crooks.me.uk>
2016-08-11 16:28:21 +0100

    DRYed db_interface

--- db/db_interface.rb
+++ db/db_interface.rb
@@ -11,35 +11,21 @@ class DbInterface
   def self.delete ( table, id = nil )
     sql = "DELETE FROM #{table}"
     sql += " WHERE id = #{ id }" if id
-    SqlRunner.run( sql )
+    return SqlRunner.run( sql )
   end
 
   def self.insert ( table, object )
-    sql_fields = "INSERT INTO #{table} ("
-    sql_values = ") VALUES ("
-    hash = to_hash( object )
-    for x, y in hash
-      if x != "id"
-        sql_fields += "#{x}, "
-        sql_values += y.is_a?( Numeric ) ? "#{y}, " : "'#{y}', "
-      end
-    end
-    sql = sql_fields[0..-3] + sql_values[0..-3] + ") RETURNING *"
+    strings = to_strings( object )
+    sql = "INSERT INTO #{table} (" + strings[ :fields ] +
+      ") VALUES (" + strings[ :values ] + ") RETURNING *"
     result = SqlRunner.run( sql ).first
     return result["id"].to_i
   end
 
   def self.update ( table, object )
-    sql_fields = "UPDATE #{table} SET ("
-    sql_values = ") = ("
-    hash = to_hash( object )
-    for x, y in hash
-      if x != "id"
-        sql_fields += "#{x}, "
-        sql_values += y.is_a?( Numeric ) ? "#{y}, " : "'#{y}', "
-      end
-    end
-    sql = sql_fields[0..-3] + sql_values[0..-3] + ") WHERE id = #{object.id} RETURNING *"
+	strings = to_strings( object )
+    sql = "UPDATE #{table} SET (" + strings[ :fields ] + ") = (" +
+      strings[ :values ] + ") WHERE id = #{object.id} RETURNING *"
     result = SqlRunner.run( sql ).first
     return result["id"].to_i
   end
@@ -48,7 +34,20 @@ class DbInterface
 
   def self.to_hash ( object )
     hash = {}
-    object.instance_variables.each {|var| hash[var.to_s.delete("@")] = object.instance_variable_get(var) }
+    object.instance_variables.each {|var| hash[var.to_s.delete("@")] =
+      object.instance_variable_get(var) }
     return hash
+  end
+  
+  def self.to_strings ( object )
+  	fields = values = ""
+    hash = to_hash( object )
+    for x, y in hash
+      if x != "id"
+        fields += "#{x}, "
+        values += y.is_a?( Numeric ) ? "#{y}, " : "'#{y}', "
+      end
+    end
+    return { :fields => fields[0..-3], :values => values[0..-3] }
   end
 end
